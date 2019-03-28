e3ee7314ea5caffa748c4583059ee21dca507994
Steve Dondley <s@dondley.com>
2019-03-28 02:41:05 -0400

    initial release

--- lib/WWW/Mechanize/Chrome/PrepareChromium.pm
+++ lib/WWW/Mechanize/Chrome/PrepareChromium.pm
@@ -1,89 +1,106 @@
 package WWW::Mechanize::Chrome::PrepareChromium ;
 
-use Moose;
-use namespace::autoclean;
-#use Moose::Role;
-#with qw( );
+use strict;
+use warnings;
+use Proc::ProcessTable;
 
-has '' => (is => '___', isa => '___', required => ___, lazy => ___, default => sub { ___ } );
-has '' => (is => '___', isa => '___', required => ___, lazy => ___, default => sub { ___ } );
+use Exporter::Easy (
+  EXPORT => [ 'prepare_chromium' ],
+);
 
-# methods here
+# prepares Chromium for use by WWW::Mechanize::Chrome
+sub prepare_chromium {
+  my $path_to_chromium = shift || '/Applications/Chromium.app/Contents/MacOS/Chromium';
+  my $port             = shift || '9222';
 
-sub BUILD {
+  # running is a port number, 'running', or 0
+  my $running = _get_chromium_status();
 
+  # chromium is running on a debugging port, so we are done
+
+  return $running if ($running =~ /\d\d+/);
+
+  # take appropriate action based on whether chrome is running
+  return $running ? _restart ($path_to_chromium, $port)
+                  : _start   ($path_to_chromium, $port);
 }
 
-### Public methods ###
+sub _get_chromium_status {
+  # get the process table
+  my $ptable  = Proc::ProcessTable->new;
 
-sub  {
-  ___
+  # look for Chromium process
+  foreach my $pr ( @{$ptable->table} ) {
+    if ($pr->fname eq 'Chromium') {
+      (my $port) = $pr->cmndline =~ /--remote-debugging-port=(\d+)/;
+      return $port || 'running';
+    }
+  }
+
+  # Chromium not running
+  return 0;
 }
 
-### Private methods ###
-
-sub  {
-  ___
+sub _start {
+  my ($path, $port) = @_;
+  my $failed = system "$path --args --remote-debugging-port=$port &";
+  die 'Could not start Chromium. Aborting.' if $failed;
+  return $port;
 }
 
+sub _quit {
+  # we call a special bash wrapper function to avoid errors from EyeTv bug
+  system ('bash', '-c', q{ source ~/.bash_profile;  osascript -e 'quit app "Chromium"'});
+  #system ('/bin/bash', '-ic', q{ osascript -e 'quit app "Chromium"'} );
+}
 
-__PACKAGE__->meta->make_immutable;
-1; # Magic true value
-# ABSTRACT: this is what the module does
+sub _restart {
+  _quit();
+  return _start(@_);
+}
+
+# ABSTRACT: prepares Chromium for use by WWW::Mechanize::Chrome
+return 1;
 
 __END__
 
 =head1 OVERVIEW
 
-Provide overview of who the intended audience is for the module and why it's useful.
+Prepare Chromium for use by L<WWW::Mechanize::Chrome>
 
 =head1 SYNOPSIS
 
   use WWW::Mechanize::Chrome::PrepareChromium;
 
+  prepare_chromium();
+
 =head1 DESCRIPTION
 
-=method method1()
+Ensures a Chromium browser has a remote debugging port available which L<WWW::Mechanize::Chrome>
+requires to work.
 
+=func prepare_chromium()
+=func prepare_chromium('', $port);
+=func prepare_chromium($path_to_chromium);
+=func prepare_chromium($path_to_chromium, $port);
 
+Starts or restarts Chrome as necessary with a debugging port. Returns the
+debugging port number if already being used or opens a port on the default
+port, C<9222> if Chrome needs to be started.
 
-=method method2()
+C<$path_to_chromium> defaults to C</Applications/Chromium.app/Contents/MacOS/Chromium>
 
+C<$port> defaults to C<9222>
 
+=head1 CONFIGURATION AND ENVIRONMENT
 
-=func function1()
-
-
-
-=func function2()
-
-
-
-=attr attribute1
-
-
-
-=attr attribute2
-
-
-
-#=head1 CONFIGURATION AND ENVIRONMENT
-#
-#WWW::Mechanize::Chrome::PrepareChromium requires no configuration files or environment variables.
-
+WWW::Mechanize::Chrome::PrepareChromium requires a working version of the
+Chromium browser.
 
 =head1 DEPENDENCIES
 
-=head1 AUTHOR NOTES
+L<Proc::ProcessTable>
 
-=head2 Development status
+=head1 SEE ALSO
 
-This module is currently in the beta stages and is actively supported and maintained. Suggestion for improvement are welcome. 
-
-- Note possible future roadmap items.
-
-=head2 Motivation
-
-Provide motivation for writing the module here.
-
-#=head1 SEE ALSO
+L<WWW::Mechanize::Chrome>
